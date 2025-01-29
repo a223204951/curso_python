76f83d27ffd76dcb09eb852d8d21bd1afd26facf
a223204951 <a223204951@unison.mx>
2025-01-29 14:45:21 -0700

    CÓDIGO PRINCIPAL JUEGO DEL GATO

---
+++ GATO/tablero.py
@@ -0,0 +1,33 @@
+'''
+TABLERO.PY: DIBUJA EL
+TABLERO DEL JUEGO DEL
+GATO :3
+'''
+
+import random
+
+def dibuja_tablero(simbolos:dict):
+    '''
+    DIBUJA EL TABLERO
+    DEL JUEGO DEL GATO
+    '''
+    print(f'''
+    {simbolos['1']} | {simbolos['2']} | {simbolos['3']}
+    ---------
+    {simbolos['4']} | {simbolos['5']} | {simbolos['6']}
+    ---------
+    {simbolos['7']} | {simbolos['8']} | {simbolos['9']}
+    ''')
+
+if __name__ == '__main__':
+    numeros = [str(i) for i in range(1,10)]
+    dsimbolos = {x:x for x in numeros}
+    dibuja_tablero(dsimbolos)
+    x = random.choice(numeros)
+    numeros.remove(x)
+    dsimbolos[x] = 'X'
+    dibuja_tablero(dsimbolos)
+    o = random.choice(numeros)
+    numeros.remove(o)
+    dsimbolos[o] = 'O'
+    dibuja_tablero(dsimbolos)
