3bd39124bbc3ff888c4dfaddf53c824bd766fae0
a223204951 <a223204951@unison.mx>
2025-02-11 14:18:19 -0700

    ACTUALIZACIÓN CÓDIGOS LÍNEA Y GATO

--- LÍNEA/funciones.py
+++ LÍNEA/funciones.py
@@ -1,29 +1,26 @@
-'''
-archivo con todas las funciones necesarias para la aplicación "linea"
-'''
-
+'''archivo con todas las funciones necesarias para la aplicación "linea" '''
 import matplotlib.pyplot as plt
 
-def calcular_y(x:float, m:float ,b:float)->float:
+def calcular_y(x:float, m:float, b:float)->float:
     '''
-        Calcula el valor de y en una línea recta
-        x: valor de x
-        m: pendiente
-        b: intersección en y regresa el valor de y
+    Calcula el valor de y en una línea recta
+    x: valor de x
+    m: pendiente
+    b: intersección en y
+    regresa el valor de y
     '''
-
     return (m*x)+b
 
 def grafica_linea(X: list, Y: list, m:float, b:float):
     '''
-    Grafica una linea recta
+    Grafica una línea recta
     X: lista de valores de x
     Y: lista de valores de y
     m: pendiente
     b: intersección en y
     '''
     plt.plot(X,Y)
-    plt.title(f'Linea recta con pendiente={m} e intersección en y={b}')
+    plt.title(f'Línea recta con pendiente={m} e intersección en y={b}')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.grid()
@@ -33,11 +30,11 @@ def test_linea():
     '''
     Comprobamos calcular_y()
     '''
-    y = calcular_y(0.0, 2.0, 3.0)
+    y  = calcular_y(0.0, 2.0, 3.0)
     return y
 
-if __name__ == "_main_":
+if __name__ == '__main__':
     if test_linea() == 3.0:
-        print("Test Exitoso :3")
+        print('Test exitoso')
     else:
-        print("Test Fallido u.u")
+        print('Test fallido')
