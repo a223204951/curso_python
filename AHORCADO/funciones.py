2ce38b1e6d21b28434590d5c1444569649c0fe73
a223204951 <a223204951@unison.mx>
2025-02-12 14:51:47 -0700

    Refactor functions in AHORCADO module for clarity and improve template loading

--- AHORCADO/funciones.py
+++ AHORCADO/funciones.py
@@ -1,34 +1,92 @@
 '''
-funciones auxiliares del juego Ahorcado
+funciones auxilaries para el juego del ahorcado
 '''
+ 
+import abc
+from random import choice
+import string
+import unicodedata
+
 
 def carga_archivo_texto(archivo:str)->list:
-    '''Carga un archivo de texto y regresa una lista con las palabras'''
-    with open(archivo, 'r', encoding='utf-8') as file:
-        oraciones = file.readlines()
+    '''Carga un arcihivo de texto y devuelve una lista con las oraciones del archivo'''
+ 
+    with open(archivo, 'r', encoding='utf-8') as f:
+        oraciones = f.readlines()
     return oraciones
-
-
-def carga_pantillas(nombre_plantilla:str)->dict:
-    '''Carga una plantilla y regresa una lista con las palabras'''
+ 
+ 
+def carga_plantillas(nombre_plantilla:str) -> dict:
+    '''
+    Carga las plantillas del juego a partir de un archivode texto
+    '''
     plantillas = {}
     for i in range(6):
-        plantillas[i] = carga_archivo_texto(f'./PLANTILLAS/{nombre_plantilla}-{i}.txt')
+        plantillas[i] = carga_archivo_texto(f'./AHORCADO/PLANTILLAS/{nombre_plantilla}-{i}.txt')
     return plantillas
-
-
-
-def despliega_plantila(diccionario:list, nivel:int):
-    '''Despliega una plantilla del juego'''
+ 
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+ 
     if nivel in diccionario:
         template = diccionario[nivel]
         for renglon in template:
             print(renglon)
-            
-if __name__ == '_main_':
-    plantillas = carga_pantillas('plantilla')
-    despliega_plantila(plantillas, 5)
-    lista_oraciones = carga_archivo_texto('./DATOS/pg15532.txt')
-    print(lista_oraciones[120:150])
-    texto = ' '.join(lista_oraciones[120:150])
-    print(texto)
+ 
+def obten_palabras(lista:str)->list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    #removemos signos de puntuacion y caracteres especiales
+    set_palabras = {palabra.strip(string.punctuation) for palabra in set_palabras}
+    #removemos números, paréntesis, corchetes, etc
+    set_palabras = {palabra for palabra in set_palabras if palabra.isalpha()}
+    #removemos acentos
+    set_palabras = {unicodedata.normalize('NFKD', palabra).encode('ASCII', 'ignore').decode('ascii') for palabra in set_palabras}
+    return list(set_palabras)
+
+def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
+    '''
+    Adivina una letra de la palabra
+    '''
+    palabra_oculta = ''
+    for letra in palabra:
+        if letra in letras_adivinadas:
+            palabra_oculta += letra
+        else:
+            palabra_oculta += '_'
+    print(f'Tienes {turnos} oportunidades de fallar')
+    print(f'El abecedario es: {abc}')
+    print(f'La palabra es: {palabra_oculta}')
+    letra = input('Dame una letra: ')
+    letra = letra.lower()
+    if letra in abc:
+        if abc[letra] == "*":
+            print('Esa letra ya la habias adivinado')
+        else:
+            abc[letra] = '*'
+            if letra in palabra:
+                letras_adivinadas.add(letra)
+            else:
+                turnos -= 1
+                print(f'La letra {letra} no está en la palabra')
+ 
+if __name__ == '__main__':
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./AHORCADO/DATOS/pg15532.txt')
+    lista_palabras = obten_palabras(lista_oraciones)
+    p = choice(lista_palabras)
+    print(p)
+    abcdario = {letra:letra for letra in string.ascii_lowercase}
+    adivinadas = set()
+    t = 5 #oportunidades
+    adivina_letra(abcdario, p, adivinadas, t)
+    adivina_letra(abcdario, p, adivinadas, t)
+    #print(lista_palabras[:50])
