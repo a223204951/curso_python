a6c004d9e8884b79e3e2a1c113009f52ee7d5ba5
a223204951 <a223204951@unison.mx>
2025-02-18 14:50:49 -0700

    Add athlete class with basic attributes and methods

---
+++ GAMES/athlete.py
@@ -0,0 +1,25 @@
+class athlete:
+    '''
+    Athlete class, with only name attribute.
+    '''
+    def __init__(self, name:str):
+        self.name = name
+    
+    def __str__(self):
+        return f"Athlete: {self.name}"
+    
+    def __repr__(self):
+        return f"Athlete: ('{self.name}')"
+    
+    def display(self):
+        print(f"{self.name}")
+
+if __name__ == "__main__":
+    a = athlete("Ana G.")
+    a.display()
+    print(a)
+    print(repr(a))
+    print(f"a: {id(a)}")
+    b = (repr(a))
+    print(b)
+    print(f"b: {id(b)}")
