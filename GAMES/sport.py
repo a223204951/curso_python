4baf7f0777d89a43aafb580e8956ad05baae2e4b
a223204951 <a223204951@unison.mx>
2025-02-19 14:46:14 -0700

    Add Sport class to represent various sports with attributes and methods

---
+++ GAMES/sport.py
@@ -0,0 +1,71 @@
+'''
+CLASE SPORT
+'''
+class Sport:
+    '''
+    Clase para representar un deporte
+    '''
+    def __init__(self, name:str, players:int, league:str):
+        '''
+        Constructor de la clase
+        '''
+        self.name = name
+        if isinstance(players, int): #Verifica que el número de jugadores sea un entero
+            self.players = players
+        else:
+            self.players = int(players) #Convierte el número de jugadores a entero
+        self.league = league
+
+    def __str__(self):
+        '''
+        Representación en cadena de la clase
+        '''
+        return f"Sport: {self.name}, Players: {self.players}, League: {self.league}"
+    
+    def __repr__(self):
+        '''
+        Representación en cadena de la clase
+        '''
+        return f"Sport(name = '{self.name}', players = {self.players}, league = '{self.league}')"
+    
+    def to_json(self)->dict:
+        '''
+        Método que convierte la clase a un diccionario
+        '''
+        return {
+            "Name": self.name,
+            "Players": self.players,
+            "League": self.league
+        }
+
+if __name__ == "__main__":
+    s = Sport("Soccer", 11, "FIFA")
+    print(s)
+    print(repr(s))
+    print(s.to_json())
+    nlf = Sport("Football", 11, "NFL")
+    lmp = Sport("Baseball", 9, "LMP")
+    mlb = Sport("Baseball", 9, "MLB")
+    lmx = Sport("Soccer", 11, "Liga MX")
+    nba = Sport("Basketball", 5, "NBA")
+    lista_deportes = [nlf, lmp, mlb, lmx, nba, s]
+    archivo_deportes = "deportes.txt"
+    with open(archivo_deportes, "w") as file:
+        for d in lista_deportes:
+            file.write(repr(d)+"\n")
+    sport_list = []
+    with open(archivo_deportes, "r") as file:
+        for line in file:
+            d = eval(line)
+            sport_list.append(d)
+    print(sport_list)
+    print(sport_list[0].to_json())
+    #Escritura de los deportes en un archivo JSON
+    import json
+    archivo_json = "deportes.json"
+    with open(archivo_json, "w") as file:
+        for d in sport_list:
+            json.dump(d.to_json(), file)
+            file.write("\n")
+    #Lectura de los deportes en un archivo JSON
+    sport_list_json = []
